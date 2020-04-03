41c34ad061833f9007e1f15d65f0fa0803ae26a6
modern-fork-2657 <kalkidan.solomon2@gmail.com>
2020-04-03 21:35:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function happyHolidays(){
   return "Happy holidays!";
-}// write your code below!
+};// write your code below!
 
 function happyHolidaysTo(name){
   return "Happy holidays `${name}`";
-}
+};
+
